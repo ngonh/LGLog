68fc32352c5cdf7ca8d122a38b6034ff75c352dc
Nguyen Huy Ngo <huyngu20@gmail.com>
2025-02-27 22:05:18 +0700

    update

---
+++ LGLog.WPFApp/Converters/MatToBitmapSourceConverter.cs
@@ -0,0 +1,32 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LGLog.WPFApp.Converters
+{
+    public class MatToBitmapSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Mat mat)
+            {
+                try
+                {
+                    var bitmapSource = mat.ToBitmapSource();
+                    mat.Dispose();
+                    return bitmapSource;
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            return Binding.DoNothing;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new object();
+        }
+    }
+}
---
+++ LGLog.WPFApp/MainWindowViewModel.cs
@@ -0,0 +1,103 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using OpenCvSharp;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LGLog.WPFApp
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        #region Constructor(s)
+        public MainWindowViewModel()
+        {
+            ContinuousCaptureCommand = new RelayCommand(ContinuousCapture);
+        }
+        #endregion
+
+        #region Properties
+        public Mat? ImageSource
+        {
+            get => imageSource;
+            set
+            {
+                imageSource = value;
+                OnPropertyChanged(nameof(ImageSource));
+            }
+        }
+        public int NG
+        {
+            get => ng;
+            set
+            {
+                ng = value;
+                OnPropertyChanged(nameof(NG));
+            }
+        }
+        public int OK
+        {
+            get => ok;
+            set
+            {
+                ok = value;
+                OnPropertyChanged(nameof(OK));
+            }
+        }
+        #endregion
+
+        #region Command(s)
+        public ICommand ContinuousCaptureCommand { get; set; }
+        #endregion
+
+        #region Method(s)
+        private async void ContinuousCapture()
+        {
+#if SIMULATION
+            Random random = new Random();
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    int imageIndex = random.Next(1, 3);
+                    string imagePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"Images/img{imageIndex}.png");
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        Mat mat = new Mat(imagePath, ImreadModes.Grayscale);
+                        if (IsOk(mat)) OK++;
+                        else NG++;
+                        App.Current.Dispatcher.Invoke(() =>
+                        {
+                            imageSource?.Dispose();
+                            ImageSource = mat.Clone();
+                        });
+
+                        mat.Dispose();
+                    }
+                    Thread.Sleep(200);
+                }
+            });
+#endif
+            // TODO Screen Capture
+        }
+        private bool IsOk(Mat imageSource)
+        {
+            string imageTemplatePath = "D:\\Workspace\\NetCoreProject\\LGLog\\LGLog.WPFApp\\Images\\NG.png";
+            if (imageSource != null) 
+            imageSource.ConvertTo(imageSource, MatType.CV_8U);
+            Mat tem = new Mat(imageTemplatePath, ImreadModes.Grayscale);
+            Mat result = new Mat();
+            tem.ConvertTo(tem, MatType.CV_8U);
+            Cv2.MatchTemplate(imageSource, tem, result, TemplateMatchModes.CCoeffNormed);
+            Cv2.MinMaxLoc(result, out _, out double maxVal, out _, out OpenCvSharp.Point maxLox);
+            bool check = maxVal > 0.91d;
+            return check;
+        }
+        #endregion
+
+        #region Field(s)
+        private Mat? imageSource;
+        private int ok = 0;
+        private int ng = 0;
+        #endregion
+    }
+}
